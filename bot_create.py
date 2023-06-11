69bf23c8cfbb4b5f0e696087c901d10713adb36b
Saveliy <s.lekhtin@innopolis.university>
2023-06-11 14:58:13 +0300

    Токен бота хранится в переменной окружения

--- bot_create.py
+++ bot_create.py
@@ -1,14 +1,10 @@
 from aiogram import Bot
-# from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
 
-# import os
-# from dotenv import load_dotenv, find_dotenv
+import os
+from dotenv import load_dotenv, find_dotenv
 
-# load_dotenv(find_dotenv())
+load_dotenv(find_dotenv())
 
-# storage = MemoryStorage()
-
-# bot = Bot(os.getenv('TOKEN'))
-bot = Bot('5811495046:AAHiqR1_XlMcrS1AqRGOacUVvC_ZvyIue2w')
+bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot)
