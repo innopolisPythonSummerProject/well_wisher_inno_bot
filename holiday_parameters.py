19d1b36df397778d11b5e6ca19cbb18b2edb9453
Milanaaaa <milanasirozhova@yandex.ru>
2023-06-17 23:38:38 +0300

    Добавлены функции /delete_birthday, /add_holiday, /delete_holiday

---
+++ holiday_parameters.py
@@ -0,0 +1,4 @@
+class Holiday:
+    def __init__(self):
+        self.is_birthday = False
+        self.holiday_name = ''
--- main_logic.py
+++ main_logic.py
@@ -9,6 +9,7 @@ from aiogram.dispatcher.filters import Text
 from bot_create import dp
 
 from bot_create import bot
+from holiday_parameters import Holiday
 
 admin_ids = [930143697, 580245280, 1089210807, 362841815]
 
@@ -16,6 +17,7 @@ engine = create_engine('sqlite:///chats.db')
 metadata = MetaData()
 Session = sessionmaker(bind=engine)
 session = Session()
+holiday = Holiday()
 
 from sqlalchemy import create_engine
 from sqlalchemy.engine import reflection
@@ -44,12 +46,13 @@ async def start_handler(message: types.Message):
 async def add_birthday(message: types.Message):
     chat_id = message.chat.id
 
-    table_name = f"table_{chat_id}"
-
     data_from_user = message.text.split(" ")
     print(data_from_user)
 
+    holiday.is_birthday = True
+
     await message.answer("Please select a date: ", reply_markup=await SimpleCalendar().start_calendar())
+
 
 async def delete_birthday(message: types.Message):
     chat_id = message.chat.id
@@ -86,43 +89,112 @@ async def delete_birthday(message: types.Message):
 #         await message.answer('Sorry, but there is no information about your birthday')
 #
 
-# simple calendar usage
+async def add_holiday(message: types.Message):
+    data_from_user = message.text.split(" ")
+    print(data_from_user)
+
+    holiday.is_birthday = False
+    holiday.holiday_name = ' '.join(data_from_user[1:])
+
+    if not holiday.holiday_name.strip():
+        await message.answer(
+            f'Please, enter /add_holiday name of holiday',
+        )
+    else:
+        await message.answer("Please select a date: ", reply_markup=await SimpleCalendar().start_calendar())
+
+
+async def delete_holiday(message: types.Message):
+    data_from_user = message.text.split(" ")
+    print(data_from_user)
+
+    holiday_name = ' '.join(data_from_user[1:])
+
+    if not holiday_name.strip():
+        await message.answer(
+            f'Please, enter /delete_holiday name of holiday',
+        )
+    else:
+
+        chat_id = message.chat.id
+
+        table_name = f"table_{chat_id}"
+        table = Table(table_name, metadata, autoload_with=engine)
+
+        select_query = table.select().where(table.c.data == holiday_name)
+        result = session.execute(select_query).fetchone()
+
+        if result:
+            await message.answer(f'{holiday_name} was successfully deleted')
+
+            delete_query = table.delete().where(table.c.data == holiday_name)
+
+            session.execute(delete_query)
+            session.commit()
+        else:
+            await message.answer('Sorry, but there is no information about this holiday')
+
+
+
 @dp.callback_query_handler(simple_cal_callback.filter())
 async def process_simple_calendar(callback_query: CallbackQuery, callback_data: dict):
     selected, date = await SimpleCalendar().process_selection(callback_query, callback_data)
     user = callback_query.from_user
-    date_str = date.strftime("%d/%m")
-    # print(date_str)
+    # date_str = date.strftime("%d/%m")
     chat_id = callback_query.message.chat.id
 
     if selected:
-        table_name = 'table_' + str(chat_id)
-        # Retrieve the table object based on the table name
-        table = Table(table_name, metadata, autoload_with=engine)
-
-        # Create the insert query and execute it
-        # columns = table.c
-        # for c in columns:
-        #     print(c.name, c.type)
-
-        select_query = table.select().where(table.c.data == user.id)
-        results = session.execute(select_query).fetchone()
-        if results:
-            await callback_query.message.answer(
-                f'{callback_query.from_user.first_name}`s birthday is rescheduled for {date.strftime("%d/%m")}',
-            )
-            update_query = (table.update().where(table.c.data == user.id).values(is_birthday=True, date=date))
-            session.execute(update_query)
-
+        if holiday.is_birthday is True:
+            await add_birthday_to_db(chat_id=chat_id, user=user, callback_query=callback_query, date=date)
         else:
-            await callback_query.message.answer(
-                f'{callback_query.from_user.first_name}`s birthday is scheduled for {date.strftime("%d/%m")}',
-            )
-            insert_query = table.insert().values(data=user.id, is_birthday= True, date=date)
-            session.execute(insert_query)
+            await add_holiday_to_db(chat_id=chat_id, holiday_name=holiday.holiday_name, callback_query=callback_query, date=date)
 
-        # Commit the transaction to save the changes
-        session.commit()
+
+async def add_birthday_to_db(chat_id, user, callback_query, date):
+    table_name = 'table_' + str(chat_id)
+    # Retrieve the table object based on the table name
+    table = Table(table_name, metadata, autoload_with=engine)
+
+    select_query = table.select().where(table.c.data == user.id)
+    results = session.execute(select_query).fetchone()
+    if results:
+        await callback_query.message.answer(
+            f'{callback_query.from_user.first_name}`s birthday is rescheduled for {date.strftime("%d/%m")}',
+        )
+        update_query = (table.update().where(table.c.data == user.id).values(is_birthday=True, date=date))
+        session.execute(update_query)
+
+    else:
+        await callback_query.message.answer(
+            f'{callback_query.from_user.first_name}`s birthday is scheduled for {date.strftime("%d/%m")}',
+        )
+        insert_query = table.insert().values(data=user.id, is_birthday=True, date=date)
+        session.execute(insert_query)
+
+    session.commit()
+
+
+async def add_holiday_to_db(chat_id, holiday_name, callback_query, date):
+    table_name = 'table_' + str(chat_id)
+    table = Table(table_name, metadata, autoload_with=engine)
+
+    select_query = table.select().where(table.c.data == holiday_name)
+    results = session.execute(select_query).fetchone()
+    if results:
+        await callback_query.message.answer(
+            f'{holiday_name} is rescheduled for {date.strftime("%d/%m")}',
+        )
+        update_query = (table.update().where(table.c.data == holiday_name).values(is_birthday=False, date=date))
+        session.execute(update_query)
+
+    else:
+        await callback_query.message.answer(
+            f'{holiday_name} is scheduled for {date.strftime("%d/%m")}',
+        )
+        insert_query = table.insert().values(data=holiday_name, is_birthday=False, date=date)
+        session.execute(insert_query)
+
+    session.commit()
 
 
 def register_handlers(dp: Dispatcher):
@@ -131,3 +203,5 @@ def register_handlers(dp: Dispatcher):
     dp.register_message_handler(add_birthday, commands='add_birthday')
     dp.register_message_handler(delete_birthday, commands='delete_birthday')
     # dp.register_message_handler(get_birthday, commands='get_birthday')
+    dp.register_message_handler(add_holiday, commands='add_holiday')
+    dp.register_message_handler(delete_holiday, commands='delete_holiday')
